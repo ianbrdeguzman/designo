3f0d7e7cbae18a5433f228266f702d5107b5edbc
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-10-17 14:09:05 +0000

    Updates

--- static/js/main.ad9d6ea9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdesigno=this.webpackJsonpdesigno||[]).push([[0],{10:function(e,t,p){},11:function(e,t,p){"use strict";p.r(t);var n=p(1),s=p.n(n),c=p(4),a=p.n(c),i=p(3),o=p.n(i),r=p(0),_=function(){return Object(r.jsx)("div",{className:o.a.app,children:Object(r.jsx)("h1",{className:o.a.app__title,children:"App"})})};p(10);a.a.render(Object(r.jsx)(s.a.StrictMode,{children:Object(r.jsx)(_,{})}),document.getElementById("root"))},3:function(e,t,p){e.exports={app:"App_app__1l8K1",app__title:"App_app__title__13sPX"}}},[[11,1,2]]]);
-//# sourceMappingURL=main.ad9d6ea9.chunk.js.map
